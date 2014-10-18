44668009f7fb69936ccc572ca977638853732c5e
babu <babuvasa@gmail.com>
2014-10-18 09:24:29 -0700

    fix configuration controller

--- app/controllers/configuration_controller.rb
+++ app/controllers/configuration_controller.rb
@@ -3,7 +3,7 @@ require 'json'
 class ConfigurationController < ApplicationController
   protect_from_forgery except: :index  
   def index
-    render json: GlobalConfiguration.where(JSON.parse(params.selection)).to_json
+    render json: GlobalConfiguration.where(JSON.parse(params[:selection] || "{}")).to_json
   end
   
   def create
