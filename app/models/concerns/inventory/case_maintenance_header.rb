4e142187a25beacac24d3a8ebf18d9e784ee0ddb
babu <babuvasa@gmail.com>
2014-11-12 19:17:40 -0800

    Shipment validtion

--- app/models/concerns/inventory/case_maintenance_header.rb
+++ app/models/concerns/inventory/case_maintenance_header.rb
@@ -10,7 +10,7 @@ module Inventory
        
        def update_case_header(app_parameters, id, fields_to_update)
          input_obj = app_parameters.merge(fields_to_update).merge(id: id).to_hash
-         if valid_app_parameters?(input_obj) && valid_data?(input_obj)
+         if  valid_data?(input_obj)
            case_hash = CaseHeader.find(id)   
            fields_to_update.each do |field, data|
             case_hash.attributes =  {field => data} 
@@ -40,13 +40,26 @@ module Inventory
    end
   
   
-  def valid_data?(fields_to_update)
+    def valid_data?(fields_to_update)
       is_valid = true
       fields_to_update.each do |field, value|
           method ="valid_#{field.to_s}?"
           is_valid = (respond_to?(method) ? send(method, fields_to_update) : true)  && is_valid                 
       end   
       is_valid
-    end 
-  end
+    end     
+  
+    def valid_shipment_nbr?(fields_to_update)
+      shipment = AsnHeader.where(client: fields_to_update.client, warehouse: fields_to_update.warehouse)
+                          .where(building: fields_to_update.building, channel: fields_to_update.channel)
+                          .where(shipment_nbr: fields_to_update.shipment_nbr).first                    
+      if shipment
+         true    
+      else
+         validation_failed('422', :shipment_nbr, 'Invalid Shipment Number')
+      end
+   end    
+ 
+ end
+  
 end
