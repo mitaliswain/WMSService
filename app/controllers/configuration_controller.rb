6db32badfef61131c0ea13985cea2fd8488dcdcb
babu <babuvasa@gmail.com>
2015-05-18 11:08:31 -0700

    configuration

--- app/controllers/configuration_controller.rb
+++ app/controllers/configuration_controller.rb
@@ -2,7 +2,9 @@ class ConfigurationController < ApplicationController
   protect_from_forgery except: :index  
   def index
     begin
-      render json: GlobalConfiguration.all #where(params[:selection]).to_json
+      filter_conditions = params[:filter_conditions]
+      configuration_hash = WmsConfiguration::ConfigurationMaintenance.new.get_configuration(basic_parameters: basic_parameters, filter_conditions: filter_conditions, expand: params[:expand])
+      render json: configuration_hash
     rescue ActiveRecord::StatementInvalid => e
       render json: {error: 'Invalid Request Parameters'}.to_json, status: '500'
     end
@@ -15,6 +17,13 @@ class ConfigurationController < ApplicationController
    rescue Exception => e
     configuration.fatal_error(e.message)
     render json: configuration.message.to_json, status: '500'
+  end
+
+  def basic_parameters
+    basic_parameter = {client: params[:client], warehouse: params[:warehouse], channel: params[:channel], building: params[:building]}
+    basic_parameter[:building] =  basic_parameter[:building].blank? ? nil : basic_parameter[:building]
+    basic_parameter[:channel] =  basic_parameter[:channel].blank? ? nil : basic_parameter[:channel]
+    basic_parameter
   end
 
 end
--- app/models/concerns/wms_configuration/configuration_maintenance.rb
+++ app/models/concerns/wms_configuration/configuration_maintenance.rb
@@ -13,8 +13,23 @@ module WmsConfiguration
       @message = {}
       @error = []
     end
-    
-     def update_configuration(app_parameters, id, fields_to_update)
+
+    def get_configuration(basic_parameters:nil, filter_conditions:nil, expand:nil)
+
+      if expand.nil?
+        #configuration_header_data = [:id, :shipment_nbr, :asn_type, :ship_via, :record_status]
+        configuration_header_data = '*'
+      else
+        configuration_header_data = '*'
+      end
+
+      configuration_headers = GlobalConfiguration.select(configuration_header_data).where(filter_conditions)
+      configuration_hash = {configuration_header: configuration_headers}
+      configuration_hash
+    end
+
+
+    def update_configuration(app_parameters, id, fields_to_update)
        input_obj = app_parameters.merge(fields_to_update).merge(id: id).to_hash
        if valid_app_parameters?(input_obj) && valid_data?(input_obj)
          configuration = GlobalConfiguration.find(id)   
