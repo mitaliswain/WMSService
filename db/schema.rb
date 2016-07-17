d08749c585758548288cce2d1ff75dd23cc27e45
mitaliswain <JagannathLenka@Jagannaths-MacBook-Pro.local>
2016-07-16 23:31:09 -0700

    debug

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151024194652) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "asn_details", force: :cascade do |t|
     t.string   "client"
     t.string   "warehouse"
